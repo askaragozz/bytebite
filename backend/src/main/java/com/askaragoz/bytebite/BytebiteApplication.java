0c0a2aafb319dddcd5d95148b7830709ec94a643
askaragozz <askaragoz55@gmail.com>
2026-04-24 09:57:33 +0300

    Add domain package structure

--- backend/src/main/java/com/askaragoz/bytebite/BytebiteApplication.java
+++ backend/src/main/java/com/askaragoz/bytebite/BytebiteApplication.java
@@ -2,8 +2,9 @@ package com.askaragoz.bytebite;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class BytebiteApplication {
 
     public static void main(String[] args) {
