524b54f9854d8387ff901fd392eb86f9d7609cb1
askaragozz <askaragoz55@gmail.com>
2026-04-24 11:41:20 +0300

    Add User entity, repository, service and controller

---
+++ backend/src/main/java/com/askaragoz/bytebite/user/User.java
@@ -0,0 +1,27 @@
+package com.askaragoz.bytebite.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name= "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
+    private LocalDateTime createdAt;
+}
